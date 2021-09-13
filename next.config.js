807547223100b5b3818457ae9c610b17cfd0dc41
Igor Bari <mucsi96@gmail.com>
2021-09-13 06:19:39 +0200

    Set base path only for GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const withMDX = require('@next/mdx')()
+const withMDX = require("@next/mdx")();
+const basePath = process.env.BASE_PATH;
 
 /** @type {import('next').NextConfig} */
 module.exports = withMDX({
-  reactStrictMode: true
-})
+  reactStrictMode: true,
+  ...(basePath && {
+    basePath,
+  }),
+});
